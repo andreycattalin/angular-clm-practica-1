e91c1aab3cf9d8622164311941062605094b0c4a
Andrey <cattalin.htc@gmail.com>
2021-03-16 12:28:39 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo3';
+
+  username = "Andrey Marin"
+
+  blueVisible = true
+  greenVisible = true
+  greenText = "Ocultar mensaje"
+
+  peliculas: string[] = [
+    "Vengadores",
+    "Deadpool",
+    "Black phanter",
+    "El becario",
+    "Sherlock",
+    "Iron man"
+  ]
+  isFilmEmpty = false
+
+  changeGreen() {
+    this.greenVisible = !this.greenVisible // si greenVisible == true lo convierte en false y al contrario
+
+    if(this.greenVisible) {
+      this.greenText = "Ocultar mensaje"
+    } else {
+      this.greenText = "Mostrar mensaje"
+    }
+  }
+
+  addFilm(data: any) {
+
+    if(data.value.trim() == "") {
+      this.isFilmEmpty = true
+      return
+    } else {
+      this.isFilmEmpty = false
+    }
+
+    this.peliculas.push(data.value)
+    data.value = ""
+  }
+
+  deleteFilm(pos: number) {
+    this.peliculas.splice(pos,1)
+  }
+
 }
